a4f185e0613fa6fac141ad235319eedf71a091b6
Emily Kearns <emilykearns2007@yahoo.com>
2019-10-03 15:45:26 -0600

    Wrote Tile interface, class, and methods.

---
+++ TomorrowDiesToday.Models/ITile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TomorrowDiesToday.Models
+{
+    interface ITile
+    {
+        bool isToggled { get; set; }
+        int alertTokens { get; set; }
+        string imageLocation { get; set; }
+        Dictionary<string, int> tileStats { get; set; }
+
+    }
+}
