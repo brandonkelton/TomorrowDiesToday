f08437e5a6ae86e5791543fd2b6760e1af74e34f
Emily Kearns <emilykearns2007@yahoo.com>
2019-10-03 16:05:43 -0600

    Changed initializer method to constructor

--- TomorrowDiesToday.Models/Squad.cs
+++ TomorrowDiesToday.Models/Squad.cs
@@ -10,12 +10,29 @@ namespace TomorrowDiesToday.Models
         public Dictionary<string, int> SquadData { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public Dictionary<string, int> SquadStats { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
-        public void InitializeSquadStats()
+        public Squad()
         {
             SquadStats.Add("Combat", 0);
             SquadStats.Add("Stealth", 0);
             SquadStats.Add("Cunning", 0);
             SquadStats.Add("Diplomacy", 0);
+
+            SquadData.Add("Thief", 0);
+            SquadData.Add("Hacker", 0);
+            SquadData.Add("Soldier", 0);
+            SquadData.Add("Assassin", 0);
+            SquadData.Add("Fixer", 0);
+            SquadData.Add("Scientist", 0);
+
+            SquadData.Add("Faced Henchmen", 0);
+
+            SquadData.Add("Hypnotic Spray", 0);
+            SquadData.Add("Explosive Rounds", 0);
+
+            SquadData.Add("Ugo Combat", 0);
+            SquadData.Add("Ugo Stealth", 0);
+            SquadData.Add("Ugo Cunning", 0);
+            SquadData.Add("Ugo Diplomacy", 0);
         }
     }
 }
