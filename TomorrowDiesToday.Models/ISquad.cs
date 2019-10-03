ef7f256aae53907f4238f5e8434b7c07731cbfe5
Emily Kearns <emilykearns2007@yahoo.com>
2019-10-03 12:49:47 -0600

    Created Squad class and interface and stat initializer

---
+++ TomorrowDiesToday.Models/ISquad.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TomorrowDiesToday.Models
+{
+    interface ISquad
+    {
+        Dictionary<string, int> SquadData { get; set; }
+        Dictionary<string, int> SquadStats { get; set; }
+    }
+}
---
+++ TomorrowDiesToday.Models/Squad.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TomorrowDiesToday.Models
+{
+    class Squad : ISquad
+    {
+
+        public Dictionary<string, int> SquadData { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Dictionary<string, int> SquadStats { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void InitializeSquadStats()
+        {
+            SquadStats.Add("Combat", 0);
+            SquadStats.Add("Stealth", 0);
+            SquadStats.Add("Cunning", 0);
+            SquadStats.Add("Diplomacy", 0);
+        }
+    }
+}
