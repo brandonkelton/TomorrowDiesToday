bd0b65daf6d5b4c4faadb84c07eef5f43a0a7db3
Kenneth Berry <rosskberry@gmail.com>
2019-10-06 17:50:55 -0600

    Add interface and initial implementation of the game service
    Add interface for game service.
    Add initial implementation of the game service.
    Register game service with IoC.

---
+++ TomorrowDiesToday.Services/Game/GameService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TomorrowDiesToday.Services.Game
+{
+    class GameService : IGameService
+    {
+        public string GameId { get; set; }
+    }
+}
---
+++ TomorrowDiesToday.Services/Game/IGameService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TomorrowDiesToday.Services.Game
+{
+    public interface IGameService
+    {
+        string GameId { get; set; }
+    }
+}
