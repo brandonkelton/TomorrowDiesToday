31bca5a225009bca82406672c4124bb949f4df50
Kenneth Berry <rosskberry@gmail.com>
2019-10-14 12:56:27 -0600

    Add inital setup for running unit tests on PlayerDataService and GameDataService Currently configured to use DynamoDB-local, however it will eventually use the mock database.

---
+++ TomorrowDiesToday.Tests/DataServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Autofac;
+using TomorrowDiesToday.Services.Data;
+using TomorrowDiesToday.Services.Data.Models;
+using TomorrowDiesToday.Services.Database;
+using TomorrowDiesToday.Models;
+using Xunit;
+
+namespace TomorrowDiesToday.Tests
+{
+    public class DataServiceTests
+    {
+        public static IContainer Container { get; private set; }
+        private static readonly ContainerBuilder _builder = new ContainerBuilder();
+
+        public DataServiceTests()
+        {
+            // RegisterAndConfigureDB();
+            RegisterAndConfigureDBLocal();
+            RegisterServices();
+            Container = _builder.Build();
+        }
+
+        /// <summary>
+        /// Use this instead of the local DB functionality if you don't want to test against an actual DB.
+        /// However, the current mocks throw an error as none of the methods are implemented, so some sort of very
+        /// basic implementation should be set up for all methods... as there are quite a few, so maybe use local for now.  ;)
+        /// </summary>
+        private void RegisterAndConfigureDB()
+        {
+            _builder.RegisterType<MockAmazonDynamoDB>().As<IAmazonDynamoDB>().SingleInstance();
+            _builder.RegisterType<MockDynamoDBContext>().As<IDynamoDBContext>().SingleInstance();
+        }
+
+        /// <summary>
+        /// Use this instead of the regular mock configuration if you want to really test against your local DB
+        /// </summary>
+        private void RegisterAndConfigureDBLocal()
+        {
+            var config = new AmazonDynamoDBConfig
+            {
+                // Replace localhost with server IP to connect with DynamoDB-local on remote server
+                ServiceURL = "http://localhost:8000/"
+            };
+
+            // Client ID is set in DynamoDB-local shell, http://localhost:8000/shell
+            var client = new AmazonDynamoDBClient("TomorrowDiesToday", "fakeSecretKey", config);
+            _builder.Register(c => client).As<IAmazonDynamoDB>().SingleInstance();
+            _builder.Register(c => new DynamoDBContext(client)).As<IDynamoDBContext>().SingleInstance();
+        }
+
+        private void RegisterServices()
+        {
+            _builder.RegisterType<DynamoClient>().As<IDBClient>().SingleInstance();
+            _builder.RegisterType<GameDataService>().As<IDataService<GameModel, GameRequest>>().SingleInstance();
+            _builder.RegisterType<PlayerDataService>().As<IDataService<PlayerModel, PlayerRequest>>().SingleInstance();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var db = Container.Resolve<IDBClient>();
+            var playerDataService = Container.Resolve<IDataService<GameModel, GameRequest>>();
+            var gameDataService = Container.Resolve<IDataService<GameModel, GameRequest>>();
+        }
+    }
+}
