35c8cc404b60696c9baf3cc06794009bf368a305
Joe Turner <joe.turner@live.com>
2019-10-16 14:03:17 -0600

    Set up interface 'Select Character' view model

--- TomorrowDiesToday/TomorrowDiesToday/ViewModels/ISelectCharacterViewModel.cs
+++ TomorrowDiesToday/TomorrowDiesToday/ViewModels/ISelectCharacterViewModel.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
+using TomorrowDiesToday.Models;
 
 namespace TomorrowDiesToday.ViewModels
 {
     public interface ISelectCharacterViewModel
     {
+        ObservableCollection<PlayerModel> Players { get; }
+
+        ICommand NextStepCommand { get; }
+        ICommand CreatePlayerCommand { get; }
+
+        string GameId { get; set; }
+        bool IsLoadingData { get; set; }
+        bool PlayerExists { get; set; }
     }
 }
