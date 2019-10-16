bac8bd7f38084a52c3df0713a5df73542fd1314b
Joe Turner <joe.turner@live.com>
2019-10-15 20:38:55 -0600

    Add 'Join Game' view model to ViewModelLocator

--- TomorrowDiesToday/TomorrowDiesToday/ViewModelLocator.cs
+++ TomorrowDiesToday/TomorrowDiesToday/ViewModelLocator.cs
@@ -9,9 +9,8 @@ namespace TomorrowDiesToday
     public class ViewModelLocator
     {
         public IMainPageViewModel MainPageViewModel => IoC.Container.Resolve<IMainPageViewModel>();
-
         public IStartPageViewModel StartPageViewModel => IoC.Container.Resolve<IStartPageViewModel>();
-
         public ICreateGameViewModel CreateGameViewModel => IoC.Container.Resolve<ICreateGameViewModel>();
+        public IJoinGameViewModel JoinGameViewModel => IoC.Container.Resolve<IJoinGameViewModel>();
     }
 }
