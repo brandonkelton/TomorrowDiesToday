f7f72c96f53b8c4e4f789309eb609a15a098c182
Joe Turner <joe.turner@live.com>
2019-10-19 08:55:02 -0600

    Set up interface for 'Navigation' service

--- TomorrowDiesToday.Services/Navigation/INavigationService.cs
+++ TomorrowDiesToday.Services/Navigation/INavigationService.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TomorrowDiesToday.Models;
+using System.Threading.Tasks;
 
 namespace TomorrowDiesToday.Services.Navigation
 {
     public interface INavigationService
     {
+
+        void PresentAsMainPage(ViewModelBase viewModel);
+
+        void PresentAsNavigatableMainPage(ViewModelBase viewModel);
+
+        Task NavigateTo(ViewModelBase viewModel);
+
+        Task NavigateBack();
+
+        Task NavigateBackToRoot();
     }
 }
