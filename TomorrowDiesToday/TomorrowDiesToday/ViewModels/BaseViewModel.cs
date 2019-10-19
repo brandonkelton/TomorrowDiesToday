6dea08c5a0c453fc04b31352e5ae2e26d8883a56
Joe Turner <joe.turner@live.com>
2019-10-19 08:53:15 -0600

    Move navigation view models to Models

--- TomorrowDiesToday/TomorrowDiesToday/ViewModels/BaseViewModel.cs
+++ TomorrowDiesToday/TomorrowDiesToday/ViewModels/BaseViewModel.cs
@@ -48,45 +48,4 @@ namespace TomorrowDiesToday.ViewModels
         #endregion
     }
 
-    public abstract class PropertyChangedBase : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void RaiseAllPropertiesChanged()
-        {
-            OnPropertyChanged(string.Empty);
-        }
-
-        private void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        protected bool SetPropertyAndRaise<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(field, newValue))
-            {
-                return false;
-            }
-
-            field = newValue;
-
-            OnPropertyChanged(propertyName);
-
-            return true;
-        }
-    }
-
-    public abstract class ViewModelBase : PropertyChangedBase, IViewModelLifecycle
-    {
-        public virtual Task BeforeFirstShown()
-        {
-            return Task.CompletedTask;
-        }
-
-        public virtual Task AfterDismissed()
-        {
-            return Task.CompletedTask;
-        }
-    }
 }
