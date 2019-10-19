11ea6c23e722c44a417a42d889092cda84116b93
Joe Turner <joe.turner@live.com>
2019-10-18 18:21:12 -0600

    Create class for 'View Locator'

---
+++ TomorrowDiesToday.Services/Navigation/ViewLocator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TomorrowDiesToday.Services.Navigation
+{
+    public class ViewLocator : IViewLocator
+    {
+    }
+}
