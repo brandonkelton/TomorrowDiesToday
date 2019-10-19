a25cb4ea1fdb2fa33919532361364cfd84bf19b0
Joe Turner <joe.turner@live.com>
2019-10-19 08:24:08 -0600

    Modify 'Base View Model' for navigation service.

--- TomorrowDiesToday/TomorrowDiesToday/ViewModels/BaseViewModel.cs
+++ TomorrowDiesToday/TomorrowDiesToday/ViewModels/BaseViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace TomorrowDiesToday.ViewModels
 {
@@ -45,5 +46,47 @@ namespace TomorrowDiesToday.ViewModels
             changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
         #endregion
+    }
+
+    public abstract class PropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaiseAllPropertiesChanged()
+        {
+            OnPropertyChanged(string.Empty);
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetPropertyAndRaise<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            field = newValue;
+
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+    }
+
+    public abstract class ViewModelBase : PropertyChangedBase, IViewModelLifecycle
+    {
+        public virtual Task BeforeFirstShown()
+        {
+            return Task.CompletedTask;
+        }
+
+        public virtual Task AfterDismissed()
+        {
+            return Task.CompletedTask;
+        }
     }
 }
