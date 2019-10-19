bfa5f0a7ae1515e7cbbab0a25ca3f6a7028576b5
Joe Turner <joe.turner@live.com>
2019-10-19 09:29:36 -0600

    Set up interface for 'View Locator' in navigation service. Add reference to Xamarin Forms.

--- TomorrowDiesToday.Services/Navigation/IViewLocator.cs
+++ TomorrowDiesToday.Services/Navigation/IViewLocator.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TomorrowDiesToday.Models;
+using Xamarin.Forms;
 
 namespace TomorrowDiesToday.Services.Navigation
 {
     public interface IViewLocator
     {
+        Page CreateAndBindPageFor<TViewModel>(TViewModel viewModel) where TViewModel : ViewModelBase;
     }
 }
