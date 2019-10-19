ac4917a15930b5c68785596203e4240f893a3b91
Joe Turner <joe.turner@live.com>
2019-10-19 08:07:53 -0600

    Create interface for 'View Model Lifecycle'

---
+++ TomorrowDiesToday/TomorrowDiesToday/ViewModels/IVeiwModelLifeCycle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomorrowDiesToday.ViewModels
+{
+    public interface IViewModelLifecycle
+    {
+        Task BeforeFirstShown();
+        Task AfterDismissed();
+    }
+}
