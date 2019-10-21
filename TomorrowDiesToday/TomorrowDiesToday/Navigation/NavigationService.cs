497a73fe7b70eb5769d6a72a64c7dcb61d18f2d2
Joe Turner <joe.turner@live.com>
2019-10-21 08:54:24 -0600

    Navigation Service

---
+++ TomorrowDiesToday/TomorrowDiesToday/Navigation/NavigationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Autofac;
+
+namespace TomorrowDiesToday.Navigation
+{
+    public class NavigationService : INavigationService
+    {
+        private INavigation Navigation { get; set; }
+
+        public NavigationService(INavigation navgationPage)
+        {
+            Navigation = navgationPage;
+        }
+
+        public async Task NavigateTo<T>() where T : Page
+        {
+            var view = IoC.Container.Resolve<T>();
+            await Navigation.PushAsync(view);
+        }
+    }
+}
