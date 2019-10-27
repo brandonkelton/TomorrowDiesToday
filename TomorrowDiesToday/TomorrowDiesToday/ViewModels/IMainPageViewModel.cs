915e9abc1123432e53bf2060fb1bac9620813fe1
Joe Turner <joe.turner@live.com>
2019-10-27 17:38:29 -0600

    Nothing works

--- TomorrowDiesToday/TomorrowDiesToday/ViewModels/IMainPageViewModel.cs
+++ TomorrowDiesToday/TomorrowDiesToday/ViewModels/IMainPageViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace TomorrowDiesToday.ViewModels
 {
     public interface IMainPageViewModel
     {
+        bool closed { get; }
+        ICommand OnPlayerClickedCommand { get; }
     }
 }
--- TomorrowDiesToday/TomorrowDiesToday/ViewModels/MainPageViewModel.cs
+++ TomorrowDiesToday/TomorrowDiesToday/ViewModels/MainPageViewModel.cs
@@ -12,5 +12,76 @@ namespace TomorrowDiesToday.ViewModels
 {
     public class MainPageViewModel : BaseViewModel, IMainPageViewModel
     {
+        #region SideSheet
+        private ConstraintExpression _sideSheetX;
+        public ConstraintExpression sideSheetX
+        {
+            get => _sideSheetX;
+            set => SetProperty(ref _sideSheetX, value);
+        }
+
+        private ConstraintExpression _sideSheetY;
+        public ConstraintExpression sideSheetY
+        {
+            get => _sideSheetY;
+            private set => SetProperty(ref _sideSheetY, value);
+        }
+
+        private ConstraintExpression _sideSheetHeight;
+        public ConstraintExpression sideSheetHeight
+        {
+            get => _sideSheetHeight;
+            private set => SetProperty(ref _sideSheetHeight, value);
+        }
+
+        private ConstraintExpression _sideSheetWidth;
+        public ConstraintExpression sideSheetWidth
+        {
+            get => _sideSheetWidth;
+            private set => SetProperty(ref _sideSheetWidth, value);
+        }
+
+        private bool _closed;
+        public bool closed
+        {
+            get => _closed;
+            private set => SetProperty(ref _closed, value);
+        }
+        #endregion
+
+        private double _sideSheetHeightFactor;
+        public double sideSheetHeightFactor
+        {
+            get => _sideSheetHeightFactor;
+            private set => SetProperty(ref _sideSheetHeightFactor, value);
+        }
+
+        public string title => "Tomorrow Dies Today";
+        public ICommand OnPlayerClickedCommand { get; private set; }
+
+        public MainPageViewModel()
+        {
+            //sideSheetHeightFactor = 1;
+            ConfigureCommands();
+        }
+
+        private void ConfigureCommands()
+        {
+            OnPlayerClickedCommand = new Command(() => OnPlayerClicked());
+        }
+
+        private void OnPlayerClicked()
+        {
+            if (closed)
+            {
+                //sideSheet.TranslateTo(Width * .75, sideSheet.Y, 100);
+                closed = false;
+            }
+            else
+            {
+                //sideSheet.TranslateTo(0, sideSheet.Y, 100);
+                closed = true;
+            }
+        }
     }
 }
