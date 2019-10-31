5048b54b9858b436e03920d47e410ad3e405a629
joshhoshiko <jhoshiko@msudenver.edu>
2019-10-30 22:45:57 -0600

    Refactored the TileModel
    Updated the TileModel to reflect the new structure.

--- TomorrowDiesToday.Models/TileModel.cs
+++ TomorrowDiesToday.Models/TileModel.cs
@@ -6,6 +6,26 @@ namespace TomorrowDiesToday.Models
 {
     public class TileModel : IModel
     {
-        public string TileId { get; set; }
+        #region Class Properties
+        public bool isFlipped { get; set; }
+        public int alertTokens { get; set; }
+        public string imageLocation { get; set; }
+        public Dictionary<string, int> tileStats { get; set; }
+        #endregion
+
+        #region Constructor(s)
+        public Tile()
+        {
+            isToggled = false;
+            alertTokens = 0;
+            imageLocation = "";
+
+            tileStats.Add("Combat", 0);
+            tileStats.Add("Stealth", 0);
+            tileStats.Add("Cunning", 0);
+            tileStats.Add("Diplomacy", 0);
+        }
+
+        #endregion
     }
 }
