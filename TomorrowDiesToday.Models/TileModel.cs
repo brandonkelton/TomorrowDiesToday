1178808c945966a923d91cfb44fb9f1133e181dc
Kenneth Berry <rosskberry@gmail.com>
2019-10-31 10:04:01 -0600

    Add TileId class property to TileModel

--- TomorrowDiesToday.Models/TileModel.cs
+++ TomorrowDiesToday.Models/TileModel.cs
@@ -7,25 +7,25 @@ namespace TomorrowDiesToday.Models
     public class TileModel : IModel
     {
         #region Class Properties
-        public bool isFlipped { get; set; }
-        public int alertTokens { get; set; }
-        public string imageLocation { get; set; }
-        public Dictionary<string, int> tileStats { get; set; }
+        public string TileId { get; set; }
+        public bool IsFlipped { get; set; }
+        public int AlertTokens { get; set; }
+        public string ImageLocation { get; set; }
+        public Dictionary<string, int> Stats { get; set; }
         #endregion
 
         #region Constructor(s)
-        public Tile()
+        public TileModel()
         {
-            isToggled = false;
-            alertTokens = 0;
-            imageLocation = "";
+            IsFlipped = false;
+            AlertTokens = 0;
+            ImageLocation = "";
 
-            tileStats.Add("Combat", 0);
-            tileStats.Add("Stealth", 0);
-            tileStats.Add("Cunning", 0);
-            tileStats.Add("Diplomacy", 0);
+            Stats.Add("Combat", 0);
+            Stats.Add("Stealth", 0);
+            Stats.Add("Cunning", 0);
+            Stats.Add("Diplomacy", 0);
         }
-
         #endregion
     }
 }
