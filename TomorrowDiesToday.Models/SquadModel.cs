a6acc94ee278db7f183065ea483009867c93b71f
joshhoshiko <jhoshiko@msudenver.edu>
2019-10-30 22:36:25 -0600

    Refactored SquadModel
    Updated SquadModel to reflect current structure

--- TomorrowDiesToday.Models/SquadModel.cs
+++ TomorrowDiesToday.Models/SquadModel.cs
@@ -8,39 +8,32 @@ namespace TomorrowDiesToday.Models
     {
         public string SquadId { get; set; }
 
-        public Dictionary<string, int> Data { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public Dictionary<string, int> Stats { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public DateTime Timestamp => throw new NotImplementedException();
+        public Dictionary<string, int> Data { get; set; }
+        public Dictionary<string, int> Stats { get; set; }
 
         public SquadModel()
         {
-            Stats.Add("Combat", 0);
-            Stats.Add("Stealth", 0);
-            Stats.Add("Cunning", 0);
-            Stats.Add("Diplomacy", 0);
-
+            //Initialize Data Dictionary
             Data.Add("Thief", 0);
             Data.Add("Hacker", 0);
             Data.Add("Soldier", 0);
             Data.Add("Assassin", 0);
             Data.Add("Fixer", 0);
             Data.Add("Scientist", 0);
-
             Data.Add("Faced Henchmen", 0);
-
             Data.Add("Hypnotic Spray", 0);
             Data.Add("Explosive Rounds", 0);
-
             Data.Add("Ugo Combat", 0);
             Data.Add("Ugo Stealth", 0);
             Data.Add("Ugo Cunning", 0);
             Data.Add("Ugo Diplomacy", 0);
+
+            //Initialize Stats Dictionary
+            Stats.Add("Combat", 0);
+            Stats.Add("Stealth", 0);
+            Stats.Add("Cunning", 0);
+            Stats.Add("Diplomacy", 0);
         }
 
-        public bool IsValid()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
