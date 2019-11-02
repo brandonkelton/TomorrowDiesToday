1acb9814d5c09a992c62c69991c622f6067adbd4
Kenneth Berry <rosskberry@gmail.com>
2019-11-02 14:53:14 -0600

    Add comments to squad model

--- TomorrowDiesToday.Models/SquadModel.cs
+++ TomorrowDiesToday.Models/SquadModel.cs
@@ -17,27 +17,42 @@ namespace TomorrowDiesToday.Models
 
         public SquadModel()
         {
-            //Initialize Data Dictionary
+            //// Initialize Data Dictionary
+
+            Data.Add("Named Henchman", 0);
+            // Named Henchmen =>
+            // 1 = Archibald Kluge
+            // 2 = Axle Robbins
+            // 3 = Azura Badeau
+            // 4 = Boris "Myasneek"
+            // 5 = Cassandra O'Shea
+            // 6 = Emmerson Barlow
+            // 7 = Jin Feng
+            // 8 = The Node
+            // 9 = Ugo Dottore
+
             Data.Add("Thief", 0);
             Data.Add("Hacker", 0);
             Data.Add("Soldier", 0);
             Data.Add("Assassin", 0);
             Data.Add("Fixer", 0);
             Data.Add("Scientist", 0);
-            Data.Add("Faced Henchmen", 0);
+            
             Data.Add("Hypnotic Spray", 0);
             Data.Add("Explosive Rounds", 0);
+
             Data.Add("Ugo Combat", 0);
             Data.Add("Ugo Stealth", 0);
             Data.Add("Ugo Cunning", 0);
             Data.Add("Ugo Diplomacy", 0);
 
-            //Initialize Stats Dictionary
+            //// Initialize Stats Dictionary
             Stats.Add("Combat", 0);
             Stats.Add("Stealth", 0);
             Stats.Add("Cunning", 0);
             Stats.Add("Diplomacy", 0);
 
+            //// Initialize as not selected
             IsSelected = false;
         }
     }
