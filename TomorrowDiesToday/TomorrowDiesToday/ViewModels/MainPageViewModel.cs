e0aa7cc513959e30f44b59652e9906d511377b8e
Joe Turner <joe.turner@live.com>
2019-11-08 19:22:37 -0700

    players listview

--- TomorrowDiesToday/TomorrowDiesToday/ViewModels/MainPageViewModel.cs
+++ TomorrowDiesToday/TomorrowDiesToday/ViewModels/MainPageViewModel.cs
@@ -7,13 +7,27 @@ using System.Windows.Input;
 using TomorrowDiesToday.Models;
 using TomorrowDiesToday.Services.Data;
 using Xamarin.Forms;
+using TomorrowDiesToday.Navigation;
+using TomorrowDiesToday.Services.Game;
 
 namespace TomorrowDiesToday.ViewModels
 {
-    public class MainPageViewModel : BaseViewModel, IMainPageViewModel
+    public class MainPageViewModel : BaseViewModel, IMainPageViewModel, IDisposable
     {
-        public MainPageViewModel()
+        private IGameService _gameService;
+        private INavigationService _navService;
+        private IDisposable _gameSubscription = null;
+        private IDisposable _playerDictSubscription = null;
+
+        public ObservableCollection<PlayerModel> Players { get; private set; } = new ObservableCollection<PlayerModel>();
+
+        public MainPageViewModel(INavigationService navigationService, IGameService gameService)
         {
+            _navService = navigationService;
+            _gameService = gameService;
+            _gameService.RequestPlayersUpdate();
+            SubscribeToUpdates();           
+
             Items = new ObservableCollection<object>
             {
                 new {Title="First"},
@@ -23,6 +37,41 @@ namespace TomorrowDiesToday.ViewModels
         }
         public ObservableCollection<object> Items { get; }
 
+        private string _gameId;
+        public string GameId
+        {
+            get => _gameId;
+            set => SetProperty(ref _gameId, value);
+        }
 
+        private string _currentPlayer;
+        public string CurrentPlayer
+        {
+            get => _currentPlayer;
+            set => SetProperty(ref _currentPlayer, value);
+        }
+
+        private void SubscribeToUpdates()
+        {
+            _gameSubscription = _gameService.ThisGame.Subscribe(gameModel =>
+            {
+                GameId = gameModel.GameId;
+                CurrentPlayer = gameModel.ThisPlayer.PlayerName;
+            });
+            _playerDictSubscription = _gameService.OtherPlayers.Subscribe(dict =>
+            {
+                Players.Clear();
+                foreach (KeyValuePair<string, PlayerModel> player in dict)
+                {
+                    Players.Add(player.Value);
+                }
+            });
+        }
+
+        public void Dispose()
+        {
+            if (_gameSubscription != null) _gameSubscription.Dispose();
+            if (_playerDictSubscription != null) _playerDictSubscription.Dispose();
+        }
     }
 }
