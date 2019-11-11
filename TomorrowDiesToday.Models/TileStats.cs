2b10ec2999d73cf5dbdb6fb6015cec68de0b6e95
Kenneth Berry <rosskberry@gmail.com>
2019-11-11 14:22:15 -0700

    Add new enums and objects from linq-examples branch

---
+++ TomorrowDiesToday.Models/TileStats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TomorrowDiesToday.Models
+{
+    public class TileStats
+    {
+        public int Combat { get; set; }
+
+        public int Cunning { get; set; }
+
+        public int Diplomacy { get; set; }
+
+        public int Stealth { get; set; }
+    }
+}
