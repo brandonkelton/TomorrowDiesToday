18c99c6e3d0e6fa4df35730fc8013cf64179f04a
Kenneth Berry <rosskberry@gmail.com>
2019-11-14 20:39:51 -0700

    Add ErrorType Enum for error messages

---
+++ TomorrowDiesToday.Models/Enums/ErrorType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace TomorrowDiesToday.Models.Enums
+{
+    public enum ErrorType
+    {
+        [Description("None")]
+        None,
+
+        [Description("Invalid Hypnotic Spray Count")]
+        InvalidHypnoticSprayCount,
+
+        [Description("Invalid Named Henchman Count")]
+        InvalidNamedHenchmanCount,
+
+        [Description("Invalid Squad Size")]
+        InvalidSquadSize,
+    }
+}
