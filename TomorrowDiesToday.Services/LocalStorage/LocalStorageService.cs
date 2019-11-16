f6849de9584def9d1b9d4ffda1945c105054a92b
Brandon Kelton <brandon.kelton@outlook.com>
2019-11-16 16:50:14 -0700

    working

---
+++ TomorrowDiesToday.Services/LocalStorage/LocalStorageService.cs
@@ -0,0 +1,24 @@
+﻿using Plugin.Settings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TomorrowDiesToday.Services.Game;
+
+namespace TomorrowDiesToday.Services.LocalStorage
+{
+    public class LocalStorageService
+    {
+        private GameService _gameService;
+
+        public LocalStorageService(GameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        public void StoreGame()
+        {
+            _gameService.
+            CrossSettings.Current.AddOrUpdateValue("GameState", "test");
+        }
+    }
+}
